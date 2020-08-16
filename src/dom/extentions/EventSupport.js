24a0bda22f57c0f8b01b54401272d1ea5c4f830f
Frank Schüler <SchuelerFrank@gmx.de>
2020-08-16 22:45:57 +0200

    beta 19

--- src/dom/extentions/EventSupport.js
+++ src/dom/extentions/EventSupport.js
@@ -1,84 +1,106 @@
 import Extender from "../../utils/Extender";
-const support = Extender("EventSupport", Prototype => {
-	const getEventHandles = element => {
-		if(!element.___EVENTHANDLES___){
-			const handles = []
+
+const DEFAULT_TIMEOUT = 100;
+const support = Extender("EventSupport", (Prototype) => {
+	const getEventHandles = (element) => {
+		if (!element.___EVENTHANDLES___) {
+			const handles = [];
 			element.___EVENTHANDLES___ = {
-				append : (events, handle, wrapper, option) => {
-					events.forEach(event => {
-						handles.push({event : event,handle : handle, wrapper : wrapper, option: option});
-						addEventListener.call(element, event, wrapper, option);			
-					});		
-				},					
-				remove : (events, handle) => {
-					const items = handles.filter(item => (!events ? true : events.indexOf(item.event) >= 0) && (!handle ? true : handle == item.handle));
-					items.forEach(item => {
+				append: (events, handle, wrapper, option) => {
+					events.forEach((event) => {
+						handles.push({ event: event, handle: handle, wrapper: wrapper, option: option });
+						addEventListener.call(element, event, wrapper, option);
+					});
+				},
+				remove: (events, handle) => {
+					const items = handles.filter((item) => (!events ? true : events.indexOf(item.event) >= 0) && (!handle ? true : handle == item.handle));
+					items.forEach((item) => {
 						const index = handles.indexOf(item);
 						removeEventListener.call(element, item.event, item.wrapper);
 						handles.splice(index, 1);
-					});	
-				}
+					});
+				},
 			};
 		}
-		
+
 		return element.___EVENTHANDLES___;
 	};
-	
-	const addEventListener = Prototype.addEventListener;	
-	Prototype.addEventListener = function(aEvent, aHandle, aOption){
-		Prototype.on.call(this, aEvent, aHandle, typeof aOption === "boolean" ? {capture : aOption, once : false, passive : false} : aOption);
+
+	const getTriggerTimeouts = (element) => {
+		if (!element.___EVENTTRIGGERTIMEOUTS___) element.___EVENTTRIGGERTIMEOUTS___ = {};
+
+		return element.___EVENTTRIGGERTIMEOUTS___;
 	};
-	
-	Prototype.on = function() {
-		if (arguments.length < 2)
-			throw new Error("Too less arguments!");
-		
+
+	const getHandleTimeouts = (element) => {
+		if (!element.___EVENTHANDLETIMEOUTS___) element.___EVENTHANDLETIMEOUTS___ = {};
+
+		return element.___EVENTHANDLETIMEOUTS___;
+	};
+
+	const addEventListener = Prototype.addEventListener;
+	Prototype.addEventListener = function (aEvent, aHandle, aOption) {
+		Prototype.on.call(this, aEvent, aHandle, typeof aOption === "boolean" ? { capture: aOption, once: false, passive: false } : aOption);
+	};
+
+	Prototype.on = function () {
+		if (arguments.length < 2) throw new Error("Too less arguments!");
+
 		const args = Array.from(arguments);
 		const events = args.shift().split(/(\s+)|(\s*,\s*)/);
 		const filter = typeof args[0] === "string" ? args.shift() : null;
 		const handle = args.shift();
-		const option = typeof args[0] !== "undefined" ? args.shift() : {capture : false, once : false, passive : false};
-		const wrapper = function(aEvent) {
-			if(filter) {
+		const option = typeof args[0] !== "undefined" ? args.shift() : { capture: false, once: false, passive: false };
+		const wrapper = function (aEvent) {
+			if (filter) {
 				const type = aEvent.target.nodeType;
-				if(!type 
-					&& (type == Node.DOCUMENT_TYPE_NODE || type == Node.DOCUMENT_FRAGMENT_NODE)
-					&& !aEvent.target.is(filter))
-					return;
+				if (!type && (type == Node.DOCUMENT_TYPE_NODE || type == Node.DOCUMENT_FRAGMENT_NODE) && !aEvent.target.is(filter)) return;
 			}
 			const result = handle.apply(handle, arguments);
-			if(option.once)
-				getEventHandles(aEvent.currentTarget).remove([aEvent.type], handle);
+			if (option.once) getEventHandles(aEvent.currentTarget).remove([aEvent.type], handle);
 			return result;
 		};
 		getEventHandles(this).append(events, handle, wrapper, option);
-		
+
 		return this;
 	};
-	
+
 	const removeEventListener = Prototype.removeEventListener;
-	Prototype.removeEventListener = function(){
+	Prototype.removeEventListener = function () {
 		return Prototype.removeOn.apply(this, arguments);
 	};
-	
-	Prototype.removeOn = function(){
+
+	Prototype.removeOn = function () {
 		const args = Array.from(arguments);
-		const events = typeof args[0] === "string"  ? args.shift().split(/(\s+)|(\s*,\s*)/) : null;
-		const handle = typeof args[0] === "function" ? args.shift() : null;		
+		const events = typeof args[0] === "string" ? args.shift().split(/(\s+)|(\s*,\s*)/) : null;
+		const handle = typeof args[0] === "function" ? args.shift() : null;
 		getEventHandles(this).remove(events, handle);
 	};
-	
-	Prototype.trigger = function() {
+
+	Prototype.trigger = function () {
 		const args = Array.from(arguments);
+		const timeout = typeof args[0] === "number" ? args.shift() : -1;
 		const type = args.shift();
-		const delegate = args[0] instanceof Event ? args.shift() : null
-		const data = args.length > 1 ? args : delegate;
-		const event = data ? new CustomEvent(type, { "bubbles": true, "cancelable": true, detail: data }) : new Event(type, { "bubbles": true, "cancelable": true });
+		const delegate = args[0] instanceof Event ? args.shift() : null;
+		const data = args.length >= 1 ? args.shift() : delegate;
+		const event = data ? new CustomEvent(type, { bubbles: true, cancelable: true, detail: data }) : new Event(type, { bubbles: true, cancelable: true });
 
-		if (delegate)
-			event.delegatedEvent = delegate;
+		if (delegate) event.delegatedEvent = delegate;
 
-		this.dispatchEvent(event);
+
+		if(timeout >= 0) {
+			const timeouts = getTriggerTimeouts(this);
+			const timeoutid = timeouts[type];
+			if(timeoutid)
+				clearTimeout(timeoutid)
+			
+			timeouts[type] = setTimeout(() => {
+				delete timeouts[type];
+				this.dispatchEvent(event);	
+			}, timeout);
+		}
+		else
+			this.dispatchEvent(event);
 		return this;
 	};
 });
--- test/tests/dom/extentions/EventSupportTest.js
+++ test/tests/dom/extentions/EventSupportTest.js
@@ -1,49 +1,94 @@
 describe("EventSupport Test", () => {
 	const container = create("<div></div>");
-	
+
 	beforeAll(() => {
 		container.append(create(window.__html__["test/sites/EventSupportTest.html"]));
 	});
-	
-	it("is \"on\" function available", () => {		
+
+	it('is "on" function available', () => {
 		expect(typeof Document.prototype.on).toBe("function");
 		expect(typeof HTMLElement.prototype.on).toBe("function");
 	});
-	
+
 	it("test addEventListener", () => {
 		let result = [
-			new Promise(r => {
-				window.addEventListener("click", event => {
+			new Promise((r) => {
+				window.addEventListener("click", (event) => {
 					expect(event).toBeDefined();
 					r();
 				});
 			}),
-			new Promise(r => {
-				document.addEventListener("click", event => {
+			new Promise((r) => {
+				document.addEventListener("click", (event) => {
 					expect(event).toBeDefined();
 					r();
 				});
-			})
+			}),
 		];
 		result = Promise.all(result);
 		document.trigger("click");
 		return result;
-	});	
-	
+	});
+
 	it("test removeOn", () => {
-	    let container = find("#remove-on");
-	    let handler = function(event){ };
-	    
-	    container.on("test", handler);	    
-	    container.removeOn("test");
-	    
-	    container.on("test", handler);     
-        container.removeOn(handler);
-        
-        container.on("test", handler);      
-        container.removeOn("test", handler);
-    }); 
-	
+		let container = find("#remove-on");
+		let handler = function (event) {};
+
+		container.on("test", handler);
+		container.removeOn("test");
+
+		container.on("test", handler);
+		container.removeOn(handler);
+
+		container.on("test", handler);
+		container.removeOn("test", handler);
+	});
+
+	it("test trigger with timeout", async () => {
+		const container = create("<div><button></button></div>").first();
+		const button = container.find("button").first();
+
+		return new Promise((resolve, reject) => {
+			const timeout = 100;
+			const timeoutid = setTimeout(reject, timeout * 2);
+
+			button.on("test", (event) => {
+				clearTimeout(timeoutid);
+				const start = event.detail;
+				const end = performance.now();
+				const diff = end - start;
+
+				container.remove();
+
+				if (timeout > diff && diff > timeout * 2) reject();
+				else resolve();
+			});
+
+			button.trigger(timeout, "test", performance.now());
+		});
+	});
+
+	it("test trigger with timeout - prevent multi trigger", async () => {
+		const container = create("<div><button></button></div>").first();
+		const button = container.find("button").first();
+
+		return new Promise((resolve, reject) => {
+			let counter = 0;
+			button.on("test", (event) => {
+				counter = counter + 1;
+			});
+
+			button.trigger(10, "test");
+			button.trigger(10, "test");
+			button.trigger(10, "test");
+			setTimeout(() => {
+				container.remove();
+				if (counter > 1) reject();
+				else resolve();				
+			}, 1000);
+		});
+	});
+
 	afterAll(() => {
 		container.remove();
 	});
