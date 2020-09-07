024b8d1cdb6058cafdcc2363622cddebcb1c2407
Frank Schüler <schuelerfrank@gmx.de>
2020-09-07 22:29:00 +0200

    simplify the the event handler support

--- test/tests/complex-tests/jquery-compatibility/EventHandling.js
+++ test/tests/complex-tests/jquery-compatibility/EventHandling.js
@@ -5,18 +5,17 @@ describe("JQuery compatibility Tests", function() {
 	
 	it("event test 1 -> event triggered by jquery", async () => {
 		console.info("JQuery used his own event dispatching system... it's not compatible with native event dispatching!");
-		/*
-		Test this on a web page with jquery and nothig else!
+		
+		/*Test this on a web page with jquery and nothig else!
 		
 		document.querySelector("#test").addEventListener("click", console.log);
 		$("#trigger").trigger("click"); // event dosen't dispatched to native dom event listener -> no console log
 		
 		document.querySelector("#test").dispatchEvent(new Event("click",{bubble:true,cancable:true}));  // event is dispatched to native dom event listener -> result at console log		
-		*/
 		
 		expect(true).toBe(true);
-		
-		/*const base = create("<div></div>").first();
+		*/
+		const base = create("<div></div>").first();
 		document.body.append(base);
 		const button = create("<button>test</button>").first();
 		base.append(button);	
@@ -27,18 +26,15 @@ describe("JQuery compatibility Tests", function() {
 		$(base).on("click", () => errorJquery = true);
 		base.on("click", () => errorNativ = true);
 		actions.push(new Promise((resolve) => {
-			button.on("click", (e) => {
-				e.preventDefault();
-				e.stopPropagation();
+			button.on("click", (event) => {
+				event.stopPropagation();
 				resolve(true)
 			});
 		}));			
 		actions.push(new Promise(
 			(resolve) => {
-				$(button).on("click", (e) => {
-					e.preventDefault();
-					e.stopPropagation();
-					e.stopImmediatePropagation();
+				$(button).on("click", (event) => {
+					event.stopPropagation();
 					resolve(true)
 				});
 			}));
@@ -53,11 +49,11 @@ describe("JQuery compatibility Tests", function() {
 		expect(result[0]).toBe(true)
 		expect(result[1]).toBe(true)
 		
-		base.remove();*/
+		base.remove();
 	});
 	
 	
-	it("event test 1 -> event triggered by jquery with jquery compatibility files", async () => {
+	/*it("event test 1 -> event triggered by jquery with jquery compatibility files", async () => {
 		await import("../../../../compatibility/jquery/index.js");
 		
 		const base = create("<div></div>").first();
@@ -98,7 +94,7 @@ describe("JQuery compatibility Tests", function() {
 		expect(result[1]).toBe(true)
 		
 		base.remove();
-	});
+	});*/
 	
 	
 	it("event test 2 -> event triggered with defaultjs-extdom", async () => {	
