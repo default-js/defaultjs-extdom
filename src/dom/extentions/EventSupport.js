7557fd9f2e82cdd9eb8cd8467a1f036cf88a0832
Frank Schüler <SchuelerFrank@gmx.de>
2021-06-21 21:05:45 +0200

    little bugfix

--- src/dom/extentions/EventSupport.js
+++ src/dom/extentions/EventSupport.js
@@ -4,11 +4,10 @@ const DEFAULT_TIMEOUT = 100;
 const support = Extender("EventSupport", (Prototype) => {
 	const EVENTSPLITER = /(\s+)|(\s*,\s*)/;
 	const getWrapperHandleMap = (element) => {
-		if (!element.__wrapperhandlemap__)
-			element.__wrapperhandlemap__ = new Map();
+		if (!element.__wrapperhandlemap__) element.__wrapperhandlemap__ = new Map();
 
 		return element.__wrapperhandlemap__;
-	}
+	};
 
 	const getTriggerTimeouts = (element) => {
 		if (!element.___EVENTTRIGGERTIMEOUTS___) element.___EVENTTRIGGERTIMEOUTS___ = {};
@@ -19,38 +18,36 @@ const support = Extender("EventSupport", (Prototype) => {
 	const removeWrapper = (element, data, eventTypes) => {
 		const { wrapper, option, events, handle } = data;
 		const capture = option.capture;
-		if(eventTypes){
+		if (eventTypes) {
 			eventTypes = typeof eventTypes === "string" ? eventTypes.split(EVENTSPLITER) : eventTypes;
 			for (let event of eventTypes) {
 				const index = events.indexOf(event);
-				if(index >= 0) {
+				if (index >= 0) {
 					element.removeEventListener(event, wrapper, capture);
 					events.splice(index, 1);
 				}
-				if(events.length == 0)				
-					getWrapperHandleMap(element).delete(handle);
+				if (events.length == 0) getWrapperHandleMap(element).delete(handle);
 			}
-			
-		}else{
+		} else {
 			for (let event of events) {
 				element.removeEventListener(event, wrapper, capture);
 			}
 			getWrapperHandleMap(element).delete(handle);
-		}		
-	}
+		}
+	};
 
-	Prototype.on = function() {
+	Prototype.on = function () {
 		if (arguments.length < 2) throw new Error("Too less arguments!");
 
 		const args = Array.from(arguments);
 		let events = typeof args[0] === "string" ? args.shift().split(EVENTSPLITER) : args.shift();
 		const filter = typeof args[0] === "string" ? args.shift() : null;
 		const handle = args.shift();
-		const option = typeof args[0] === "undefined" ? { capture: false, once: false, passive: false } : (typeof args[0] === "boolean" ? { capture: args.shift(), once: false, passive: false } : args.shift());
-		const wrapper = function(aEvent) {
+		const option = typeof args[0] === "undefined" ? { capture: false, once: false, passive: false } : typeof args[0] === "boolean" ? { capture: args.shift(), once: false, passive: false } : args.shift();
+		const wrapper = function (event) {
 			if (filter) {
 				const target = event.target;
-				if (typeof target.is === "function" && !aEvent.target.is(filter)) return true;
+				if (typeof target.is === "function" && !target.is(filter)) return;
 			}
 			const result = handle.apply(null, arguments);
 			if (option.once) removeWrapper(this, wrapper);
@@ -66,18 +63,15 @@ const support = Extender("EventSupport", (Prototype) => {
 		return this;
 	};
 
-
-	Prototype.removeOn = function(handle, event, capture) {
+	Prototype.removeOn = function (handle, event, capture) {
 		const data = getWrapperHandleMap(this).get(handle);
-		if (data) 
-			removeWrapper(this, data, event);
-		else
-			this.removeEventListener(handle, event, capture);
+		if (data) removeWrapper(this, data, event);
+		else this.removeEventListener(handle, event, capture);
 
 		return this;
 	};
 
-	Prototype.trigger = function() {
+	Prototype.trigger = function () {
 		const args = Array.from(arguments);
 		const timeout = typeof args[0] === "number" ? args.shift() : -1;
 		if (timeout >= 0) {
@@ -94,7 +88,7 @@ const support = Extender("EventSupport", (Prototype) => {
 			const type = args.shift();
 			const delegate = args[0] instanceof Event ? args.shift() : null;
 			const data = args.length >= 1 ? (args.length == 1 ? args.shift() : args) : delegate;
-			const event = data ? new CustomEvent(type, { bubbles: true, cancelable: true, composed: true, detail: data }) : new Event(type, { bubbles: true, cancelable: true, composed: true, });
+			const event = data ? new CustomEvent(type, { bubbles: true, cancelable: true, composed: true, detail: data }) : new Event(type, { bubbles: true, cancelable: true, composed: true });
 
 			if (delegate) event.delegatedEvent = delegate;
 			this.dispatchEvent(event);
